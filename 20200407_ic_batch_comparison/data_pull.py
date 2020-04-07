153d05d3dc9039f78a3700789b7c4906934c4353
Jesse Montgomery <jlmontie@gmail.com>
2020-04-07 17:27:24 -0600

    fix

--- 20200407_ic_batch_comparison/data_pull.py
+++ 20200407_ic_batch_comparison/data_pull.py
@@ -12,7 +12,9 @@ def get_accession_dict(df):
     return accession_dict
 
 
-project_dir = '/srv/idbydna-group3/results/idbd_rnd_v2/'
+project_dir_1 = '/srv/idbydna-group3/results/idbd_rnd_v2/'
+project_dir_2 = '/srv/idbydna-group2/results/arup-resp-prod/'
+project_dirs = [project_dir_1, project_dir_2]
 
 batch_info = pd.read_csv('ic_batch_info.csv')
 batch_new = batch_info[batch_info['ic_batch'] == 'new']
@@ -20,7 +22,10 @@ batch_new_dict = get_accession_dict(batch_new)
 batch_old = batch_info[batch_info['ic_batch'] == 'old']
 batch_old_dict = get_accession_dict(batch_old)
 
-extractor_old = DataExtractor(project_dir=project_dir,
-    accession_dict=batch_old_dict, lib_type='rna')
-df_old = extractor_old.collect_data()
+df_old_ls = []
+for project_dir in project_dirs:
+    extractor_old = DataExtractor(project_dir=project_dir,
+        accession_dict=batch_old_dict, lib_type='rna')
+    df_old_ls.append(extractor_old.collect_data())
+df_old = pd.concat(df_old_ls)
 print(df_old)
